da11db54f0f434d651d0377431f119facef31a06
Jakub Josef Krása <jjkrasa@gmail.com>
2025-09-11 18:26:52 +0200

    Initial commit

---
+++ src/main/java/com/fourstory/fourstory_api/FourstoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.fourstory.fourstory_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourstoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourstoryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fourstory/fourstory_api/FourstoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourstory.fourstory_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourstoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
