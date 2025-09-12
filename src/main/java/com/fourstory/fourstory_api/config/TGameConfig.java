988822975939335336c36df3a3f95975ea104141
Jakub Josef Krása <jjkrasa@gmail.com>
2025-09-12 13:34:04 +0200

    database connection config for TGAME_GSP and TGLOBAL_GSP MSSQL databases

---
+++ src/main/java/com/fourstory/fourstory_api/config/TGameConfig.java
@@ -0,0 +1,51 @@
+package com.fourstory.fourstory_api.config;
+
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "com.fourstory.fourstory_api.repository.tgame",
+        entityManagerFactoryRef = "tgameEntityManagerFactory",
+        transactionManagerRef = "tgameTransactionManager"
+)
+public class TGameConfig {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.tgame")
+    public DataSourceProperties tgameDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource tgameDataSource() {
+        return tgameDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean(name = "tgameEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean tgameEntityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(tgameDataSource());
+        emf.setPackagesToScan("com.fourstory.fourstory_api.model.tgame");
+        emf.setPersistenceUnitName("tgame");
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        return emf;
+    }
+
+    @Bean(name = "tgameTransactionManager")
+    public PlatformTransactionManager tgameTransactionManager(EntityManagerFactory tgameEntityManagerFactory) {
+        return new JpaTransactionManager(tgameEntityManagerFactory);
+    }
+}
---
+++ src/main/java/com/fourstory/fourstory_api/config/TGlobalConfig.java
@@ -0,0 +1,50 @@
+package com.fourstory.fourstory_api.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "com.fourstory.fourstory_api.repository.tglobal",
+        entityManagerFactoryRef = "tglobalEntityManagerFactory",
+        transactionManagerRef = "tglobalTransactionManager"
+)
+public class TGlobalConfig {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.tglobal")
+    public DataSourceProperties tglobalDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource tglobalDataSource() {
+        return tglobalDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean(name = "tglobalEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean tgameEntityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(tglobalDataSource());
+        emf.setPackagesToScan("com.fourstory.fourstory_api.model.tglobal");
+        emf.setPersistenceUnitName("tglobal");
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        return emf;
+    }
+
+    @Bean(name = "tglobalTransactionManager")
+    public PlatformTransactionManager tglobalTransactionManager(EntityManagerFactory tgameEntityManagerFactory) {
+        return new JpaTransactionManager(tgameEntityManagerFactory);
+    }
+}
